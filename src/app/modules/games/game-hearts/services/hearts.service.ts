00b2948dc8e1ca31778fe5f65effc56e8a740411
marbwoulret <bramterlouw12@gmail.com>
2023-01-15 21:17:24 +0100

    added methods for the game of hearts to the service

--- src/app/modules/games/game-hearts/services/hearts.service.ts
+++ src/app/modules/games/game-hearts/services/hearts.service.ts
@@ -30,11 +30,68 @@ export class HeartsService {
 
 
   /**
+   * dbInitGame
+   * * Method to initialize a new game of hearts in Firestore/
+   * 
+   */
+  dbInitGame = () => {
+    // TODO: Create a new game in firebase with deck and hand data.
+  };
+
+
+
+  /**
    * joinGame
    * * Method to join an existing game of hearts.
    * 
    */
   joinGame = () => {
     // TODO: Get game data from firebase and join a game.
+  };
+
+
+
+  /**
+   * startGame
+   * * Method to start the game of hearts.
+   * 
+   */
+  startGame = () => {
+    // TODO: Start a game of hearts when all players have joined.
+  };
+
+
+
+  /**
+   * submitCard
+   * * Method to submit a card to the game.
+   * 
+   */
+  submitCard = () => {
+    // TODO: Submit a card to the game.
+  };
+
+
+
+  /**
+   * getRoundData
+   * * Method to get the round data from firebase.
+   * 
+   */
+  getRoundData = () => {
+    // TODO: Get the round data from firebase.
+    // TODO: Calcolate player with highest card.
+    // TODO: Calculate points of round
+  };
+
+
+
+  /**
+   * dbUpdateScore
+   * * Method to update the score in firebase.
+   * 
+   */
+  dbUpdateScore = () => {
+    // TODO: Update the score in firebase.
   };
 }
